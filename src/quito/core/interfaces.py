eb63200508cfea9591e3786c7ca44850ded543bb
phillipj1514 <phillipjunior95@gmail.com>
2019-07-26 01:13:49 -0500

    fixes to dockerignore

--- src/quito/core/interfaces.py
+++ src/quito/core/interfaces.py
@@ -2,8 +2,6 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-from zope.interface import Interface
-from zope import schema
 
 class IQuitoCoreLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
